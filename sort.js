53ff44643e58fd9f4526aa231dd61b1ec3e209c0
Ashchal Shrestha <ashchal93@gmail.com>
2021-03-09 08:50:48 +0545

    aa

---
+++ sort.js
@@ -0,0 +1,24 @@
+const normalSort = (array) => {
+  console.log(array.sort());
+};
+
+const ascendingSort = (array) => {
+  console.log(
+    array.sort(function (a, b) {
+      return a - b;
+    })
+  );
+};
+
+const descendingSort = (array) => {
+  console.log(
+    array.sort(function (a, b) {
+      return b - a;
+    })
+  );
+};
+
+a1 = [5, 10, 2, 2, 6, 9, 2, 4, 1, 1, 0, 8, 3, 9, 4, 0, 5, 3, 2, 0, 1, 7];
+normalSort(a1);
+ascendingSort(a1);
+descendingSort(a1);
